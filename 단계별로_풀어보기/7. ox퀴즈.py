3d6636e78644cb110b0aa7996945e5d872aa5c26
huik95 <b100hhss@gmail.com>
2021-04-26 19:52:21 +0900

    .

---
+++ 단계별로_풀어보기/7. ox퀴즈.py
@@ -0,0 +1,12 @@
+n = int(input())
+for i in range(n):
+    a = list(input())
+    sum = 0
+    b = 1
+    for i in a:
+        if i == 'O':
+            sum += b
+            b += 1
+        else:
+            b = 1
+    print(sum)
